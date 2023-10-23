14849e5f05c099a0452be3574fc8a44e85f8f22f
leona <u202018777@upc.edu.oe>
2023-10-23 15:42:32 -0500

    added railway database.

--- src/main/java/com/example/demo/shared/configuration/OpenApiConfiguration.java
+++ src/main/java/com/example/demo/shared/configuration/OpenApiConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class OpenApiConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200/").allowedMethods("*").allowedHeaders("*");
+        registry.addMapping("/**").allowedOrigins("https://demoapp-8aec6.web.app/").allowedMethods("*").allowedHeaders("*");
     }
     @Bean
     public OpenAPI customOpenAPi(
